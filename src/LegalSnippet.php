387dc51508f998f97e4be3a9f54abc461fcf8a6b
Hudhaifa Shatnawi <hudhaifa.shatnawi@gmail.com>
2018-04-25 15:18:07 -0500

    - Upgrading to SS4 & BS4...   * Used TemplateGlobalProvider instead of Controller extension, so $LegalSnippet can be use anywhere in any template file.

--- src/LegalSnippet.php
+++ src/LegalSnippet.php
@@ -1,6 +1,7 @@
 <?php
 
 use SilverStripe\ORM\DataObject;
+use SilverStripe\View\TemplateGlobalProvider;
 
 /**
  *
@@ -8,7 +9,8 @@ use SilverStripe\ORM\DataObject;
  * @version 1.0, Apr 25, 2018 - 2:44:39 PM
  */
 class LegalSnippet
-        extends DataObject {
+        extends DataObject
+        implements TemplateGlobalProvider {
 
     private static $db = [
         'Text' => 'Varchar(255)'
@@ -16,5 +18,28 @@ class LegalSnippet
     private static $has_one = [
         'LegalPage' => LegalPage::class
     ];
+
+    public static function get_random_snippet() {
+        $randomPage = LegalPage::get()->sort("RAND()")->limit("1")->first();
+        $snippet = $randomPage->Snippets()->sort("RAND()")->limit("1")->first();
+
+        if (!$snippet) {
+            return;
+        }
+
+        return $snippet
+                        ->customise([
+                            'Title' => $randomPage->getTitle(),
+                            'Link' => $randomPage->Link(),
+                            'Snippet' => $snippet->Text
+                        ])
+                        ->renderWith('Includes/LegalSnippet');
+    }
+
+    public static function get_template_global_variables(): array {
+        return array(
+            'LegalSnippet' => 'get_random_snippet',
+        );
+    }
 
 }
